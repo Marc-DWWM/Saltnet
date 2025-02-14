88da8fdbc2371c15d98907e3ed6e190546513a9f
Mhoudoin <marceahoudoin@gmail.com>
2025-02-14 16:56:22 +0100

    On commence à faire la page principal une fois connecté, création du crud post

---
+++ src/Entity/Post.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $user_post = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $post = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserPost(): ?int
+    {
+        return $this->user_post;
+    }
+
+    public function setUserPost(int $user_post): static
+    {
+        $this->user_post = $user_post;
+
+        return $this;
+    }
+
+    public function getPost(): ?string
+    {
+        return $this->post;
+    }
+
+    public function setPost(string $post): static
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
