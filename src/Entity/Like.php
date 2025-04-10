e2dd78355fc3ffff0135c0ef001e9934246c4c1a
Mhoudoin <marceahoudoin@gmail.com>
2025-04-10 11:18:12 +0200

    modif a entity like

--- src/Entity/Like.php
+++ src/Entity/Like.php
@@ -6,9 +6,10 @@ use App\Repository\LikeRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: LikeRepository::class)]
-#[ORM\Table(name: '`like`', uniqueConstraints: [
-    new ORM\UniqueConstraint(name: "user_post_unique", columns: ["user_like_id", "post_like_id"])
-])]
+#[ORM\Entity]
+#[ORM\Table(name: 'like')]
+#[ORM\UniqueConstraint(name: 'user_post_unique', columns: ['user_id', 'post_id'])]
+
 class Like
 {
     #[ORM\Id]
